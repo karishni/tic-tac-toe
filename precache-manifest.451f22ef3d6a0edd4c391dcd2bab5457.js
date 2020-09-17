d32f76a3ef93d405c38106f3623c9b9ab76a99f4
Karishni Mehta <karishni25@gmail.com>
2020-09-17 22:25:54 +0530

    Updates

---
+++ precache-manifest.451f22ef3d6a0edd4c391dcd2bab5457.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07b598b5b52e764e3370dbd9a0dc95db",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "2abadd82fa9573f06c4d",
+    "url": "/tic-tac-toe/static/css/main.0f3284b4.chunk.css"
+  },
+  {
+    "revision": "5f09379b5ff20bc77d6d",
+    "url": "/tic-tac-toe/static/js/2.abc7a5eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.abc7a5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abadd82fa9573f06c4d",
+    "url": "/tic-tac-toe/static/js/main.9830c4bd.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
